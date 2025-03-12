b23b1d0592b77ebd4fd205b54db1ca4ca56ddc63
Precious-Israel <carisabiodun@gmail.com>
2025-03-12 13:00:09 +0100

    commit all

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -7,3 +7,14 @@ class Program
         Console.WriteLine("Hello World! This is the Resumes Project.");
     }
 }
+public class Job 
+{public double _resposibilties;
+  public double _behaviors;
+
+}
+
+public class Resumes
+{public double _resposibilties;
+  public double _behaviors;
+    
+}
