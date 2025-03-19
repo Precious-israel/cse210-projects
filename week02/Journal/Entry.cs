3a6c28bd88ef12e38d5d1e7f33179d20b21dfea2
Precious-Israel <carisabiodun@gmail.com>
2025-03-19 10:34:48 +0100

    update changes

---
+++ week02/Journal/Entry.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Entry
+{
+    public string Date { get; set; }
+    public string Title { get; set; }
+    public string Prompt { get; set; }
+    public string Response { get; set; }
+    public string Mood { get; set; }
+    public string Tags { get; set; } // Comma-separated tags
+}
